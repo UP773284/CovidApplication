32cd43b31009a7267824782f51b2b6c7e37d27fe
Mubanga Chisanga <up773284@myport.ac.uk>
2021-04-14 00:04:08 +0100

    backend login code

--- CovidApp/app/src/main/java/com/example/covidapp/MainActivity.java
+++ CovidApp/app/src/main/java/com/example/covidapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.covidapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,14 +16,27 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        Email = (EditText)findViewById(R.id.etEmail);
+        Password = (EditText)findViewById(R.id.tvPassword);
+        Login = (Button)findViewById(R.id.buttonLogin);
+
+        Login.setOnClickListener(new View.onClickListener() {
+            public void onClick(View view) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -26,5 +44,12 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
+    private void validate(String userName, String userPassword){
+        if ((userName == "test@myport.ac.uk") && (userPassword == "smellyjelly")) {
+            Intent intent = new Intent(MainActivity.this, secondActivty.class);
+            startActivity(intent);
+        }else{
+            System.out.println("Incorrect email or password! Please Try again");
+        }
+    }
 }
