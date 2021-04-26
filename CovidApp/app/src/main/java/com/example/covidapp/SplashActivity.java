864ee4e444fbadcfdf72c82393b0adb4c91e427b
Mubanga Chisanga <up773284@myport.ac.uk>
2021-04-26 18:06:03 +0100

    Backend code for splash screen

--- CovidApp/app/src/main/java/com/example/covidapp/SplashActivity.java
+++ CovidApp/app/src/main/java/com/example/covidapp/SplashActivity.java
@@ -10,5 +10,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+                val intent = Intent(PackageContext: this@content_sign_up::class.java)
+                startActivty(Intent)
+        }, delayMillis:3000)
     }
 }
